989c9bfafa7c5930b51d5f8a1535f63b840840d8
Olyazarichna <olya.zarichna@gmail.com>
2022-08-15 08:06:54 +0300

    Update MoviesGallery.jsx

--- src/MoviesGallery/MoviesGallery.jsx
+++ src/MoviesGallery/MoviesGallery.jsx
@@ -1,14 +1,13 @@
-import { nanoid } from 'nanoid';
 import { FilmCard } from '../FilmCard/FilmCard';
 
 export const MoviesGallery = ({ movies, handleStatus, handleModal }) => {
   return movies.map(({ id, ...restProps }) => (
     <FilmCard
-      key={id + nanoid()}
+      key={id}
       id={id}
       movie={restProps}
       handleStatus={handleStatus}
       handleModal={handleModal}
     />
   ));
-};;
+};
